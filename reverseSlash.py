0997a07a331d0d3e61c59da063bdf2add69df045
Ali Cem Sönmez <snmzcm@gmail.com>
2022-11-06 22:58:45 +0300

    Bug Fix
    Fixed ":" Bug
    Added /mnt/ in front of path so I can use It quickly in WSL

--- reverseSlash.py
+++ reverseSlash.py
@@ -2,13 +2,18 @@ def main():
     thePath = str(input("Enter Windows Path: "))
     listPath = list(thePath)
     char =  '\\'
+    char2 = ':'
     newlist = ""
     for i, n in enumerate(listPath):
         if n == char:
             listPath[i] = "/"
             newlist = ''.join(listPath)
+        elif n == char2:
+            listPath[i] = ""
+            newlist = ''.join(listPath)
+
         
-    print(newlist)
+    print("" + "/mnt/" + newlist.lower())
 
     return 0;
 
